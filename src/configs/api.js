24325e56dc2218acb9b0ccdcba4b8bde4503c408
hesamoon <108687260+hesamoon@users.noreply.github.com>
2024-12-22 17:14:52 +0330

    add token

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
+    "Authorization": "Bearer 7fb412bd-a074-4daf-9a4b-02138771d3b6",
     "Content-Type": "application/json",
   },
 });
