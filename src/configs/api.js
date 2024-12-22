249ae0cd07860034592a97b343a87b079fdbae26
hesamoon <108687260+hesamoon@users.noreply.github.com>
2024-12-22 20:28:51 +0330

    add token in Template Literals way

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
-    "Authorization": "Bearer 7fb412bd-a074-4daf-9a4b-02138771d3b6",
+    "Authorization": `Bearer ${import.meta.env.RAILWAY_TOKEN}`,
     "Content-Type": "application/json",
   },
 });
